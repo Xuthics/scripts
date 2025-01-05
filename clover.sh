d690584d4a90b5bec407a4d1296e958f1528f95f
Xuthics <xyx4040@gmail.com>
2025-01-06 06:51:35 +0800

    Update clover.sh

--- clover.sh
+++ clover.sh
@@ -5,9 +5,10 @@ docker pull xuthics/clover-trader:latest
 
 # 创建配置目录
 mkdir -p config
+mkdir -p keys
 
 # 运行 Docker 容器，并设置自动重启和容器名称
-docker run --name clover-trader --restart unless-stopped -v $(pwd)/config:/app/config -d xuthics/clover-trader:latest
+docker run --name clover-trader --restart unless-stopped -v $(pwd)/config:/app/config -v $(pwd)/keys:/app/keys -d xuthics/clover-trader:latest
 
 echo "Clover Trader 已成功安装并运行。"
 
