9613860de9ca3c5e5a4b4037cea050f7a6280169
Xuthics <xyx4040@gmail.com>
2025-03-18 15:29:14 +0800

    Update clover-debug.sh

--- clover-debug.sh
+++ clover-debug.sh
@@ -10,7 +10,7 @@ mkdir -p keys
 docker stop clover-trader
 docker rm -f clover-trader 2>/dev/null
 # 运行 Docker 容器，并设置自动重启和容器名称
-docker run --name clover-trader --restart unless-stopped -v $(pwd)/config:/app/config -v $(pwd)/keys:/app/keys -d xuthics/clover-trader:latest
+docker run --name clover-trader --restart unless-stopped -v $(pwd)/config:/app/config -v $(pwd)/keys:/app/keys -d xuthics/clover-trader:debug
 echo "[DEBUG] Clover Trader 已成功安装并运行。"
 
 # 打开 Docker 容器的日志
